c99097195109f4dfbadb549d6236f1681ee56ad8
Thalia Vilela <thalia13vf@gmail.com>
2020-10-20 01:43:22 -0200

    Segundo commit

--- src/main/java/com/biopokedex/biopokedex/controller/BiopokedexController.java
+++ src/main/java/com/biopokedex/biopokedex/controller/BiopokedexController.java
@@ -41,15 +41,13 @@ public class BiopokedexController {
 				"Bacterias gram-negativas responsáveis pela lixiviação de metais, formando ions férricos (Fe3+) e ácido sulfúrico, estas bactérias são capazes de realizar lixiviação indireta, ocorrendo regeneração do sulfato férrico e também produzir ácido sulfúrico a partir do enxofre por meio de ações biológicas. O ácido sulfúrico é muito importante, pois é responsável por manter a faixa de pH favorável para o desenvolvimento das bactérias.", "Leptocell.jpg"));
 		
 		lista.add(new BioLixiviador(2,"Acidithiobacillus thiooxidans","Composto Químico", "Proteobacteria", "Acidithiobacillales", "Acidithiobacilliaceae", "bactéria gram-negativa que uso enxofre como fonte de energia e para conseguir é capaz de lixiviar alguns metais e minerais. Sendo importante para a diminuição de alguns metais tóxicos como cobalto e lítio.", "thiooxidans.png"));
-		lista.add(new BioLixiviador(3, "Acidithiobacillus ferrooxidans", "Metal", "Proteobacteria", "Acidithiobacillia", "Acidithiobacilliaceae", " Bactérias aeróbias fixadoras de dióxido de carbono; Têm seu pH ótimo na faixa entre 2 e 3; São conhecidas pela catalisação da oxidação do Fe2+ à Fe3+;Utilizam-se de substratos inorgânicos (sulfatos ou cátions metálicos reduzidos) como fonte de energia (França et al., 2004); Possui resistência elevada à altas concentrações de metais tóxicos (cromo, níquel, manganês) (Novo, 1998); França et al., 2004, observaram remoções entre 40% e 85% em lodos de esgoto para zinco, cromo, níquel, cádmio, cobre e chumbo (França et al., 2004); Reação de biolixiviação de metais\r\n" + 
-				"\r\n" + 
+		lista.add(new BioLixiviador(3, "Acidithiobacillus ferrooxidans", "Metal", "Proteobacteria", "Acidithiobacillia", "Acidithiobacilliaceae", " Bactérias aeróbias fixadoras de dióxido de carbono; Têm seu pH ótimo na faixa entre 2 e 3; São conhecidas pela catalisação da oxidação do Fe2+ à Fe3+;Utilizam-se de substratos inorgânicos (sulfatos ou cátions metálicos reduzidos) como fonte de energia (França et al., 2004); Possui resistência elevada à altas concentrações de metais tóxicos (cromo, níquel, manganês) (Novo, 1998); França et al., 2004, observaram remoções entre 40% e 85% em lodos de esgoto para zinco, cromo, níquel, cádmio, cobre e chumbo (França et al., 2004); Reação de biolixiviação de metais\r\n" +  
 				"A.ferrooxidans\r\n" + 
-				"Processo indireto:            Fe2+ + 0,5 O2 + 2 H+ → Fe3+ + H2O \r\n" + 
-				"                            4Fe3+ + MS + 2H2O + O2 → M2+ + 4Fe2+ + SO42- + 4H+\r\n" + 
-				"\r\n" + 
+				"Processo indireto:  Fe2+ + 0,5 O2 + 2 H+ → Fe3+ + H2O \r\n" + 
+				" 4Fe3+ + MS + 2H2O + O2 → M2+ + 4Fe2+ + SO42- + 4H+\r\n" + 
 				"A.thiooxidans\r\n" + 
-				"Processo direto:                       MS + 2O2 → M2+ + SO42-\r\n" + 
-				"", "ferrooxidans.png"));
+				"Processo direto:                       MS + 2O2 → M2+ + SO42-\r\n" 
+				,"ferrooxidans.png"));
 		lista.add(new BioLixiviador(4, "Sulfobacillus thermosulfidooxidans", "Metal", "Proteobacteria", "Sulfobacillus", "Alicyclobacillaceae", " Termófilas moderadas, possibilitando processos em temperaturas elevadas (50°C); pH ótimo de 1,7; São heterotróficas, e conhecidas por oxidarem Fe2+ e enxofre elementar; Utilizados para a recuperação de metais em baixos teores, como no caso de pilhas e minérios (jarositas e calcopiritas) (SICUPIRA, 2011).", "thermosulfidooxidans.png"));
 		lista.add(new BioLixiviador(5, "?", "?", "?", "?", "?", "?", "novas.png"));
 		lista.add(new BioLixiviador(6, "?", "?", "?", "?", "?", "?", "novas.png"));
